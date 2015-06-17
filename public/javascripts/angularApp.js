b2e43716603361e945717b7a17bd3108c6d7ebd9
Nick Redmond <nicholas.redmond@ymail.com>
2015-06-16 19:53:31 -0600

    Added AZ to list of available states during signup.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -8,7 +8,7 @@ var USER_LINK_TEMPLATE = '#URL here:::link text here#'
 //--- HELPER FUNCTIONS ---//
 function populateStates(){
 	var statesList = [
-			/*'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'D.C.', 'DE', 'FL',
+			/*'AL', 'AK',*/ 'AZ',/* 'AR', 'CA', 'CO', 'CT', 'D.C.', 'DE', 'FL',
 			'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
 			'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE',
 			'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
